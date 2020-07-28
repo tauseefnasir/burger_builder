e3d2ce0caea3b4a9f1885e4c21a659fdc2929011
tauseefnasir <tauseefnasir501@gmail.com>
2020-07-28 10:34:11 +0500

    basic structure

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./Layout.module.css"
+const layout = (props) => (
+  <>
+    <div>toolbar, sidedrawer, backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </>
+);
+
+export default layout;
